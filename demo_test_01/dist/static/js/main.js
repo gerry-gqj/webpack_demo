6effab05ca57c49f735072530cc9ac6a3d63d61a
gerry-gqj <gerry_gqj@163.com>
2022-09-06 22:49:10 +0800

    # delete dist

--- demo_test_01/dist/static/js/main.js
+++
@@ -1,2 +0,0 @@
-!function(){"use strict";let e=2+1;console.log(e);let n=function(){for(var e=arguments.length,n=new Array(e),o=0;o<e;o++)n[o]=arguments[o];return n.reduce(((e,n)=>e+n),0)}(1,2,3,4);console.log(n)}();
-//# sourceMappingURL=main.js.map
